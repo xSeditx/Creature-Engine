7cbed11c361877959ccb64b7e5c367c132c47796
Curtis Turner <curtis.turner@live.com>
2019-12-02 19:38:47 -0500

    Finished Threadpool

--- CreatureEngine/Core/Threading/Threadpool.h
+++ CreatureEngine/Core/Threading/Threadpool.h
@@ -4,105 +4,229 @@
 #include <future>
 #include <deque>
 #include <queue>
+#include <array>
+#include <tuple>
 
-typedef void(*Worker_Function)();
-// NOTE: https://riptutorial.com/cplusplus/example/15806/create-a-simple-thread-pool
+#include<Windows.h>
+#include"Core\Common.h"
+#include<iostream>
+
+#pragma warning( push )
+#pragma warning( disable : 4244 )
+#pragma warning( disable : 4018 ) // Optimization off warning of mine
+
+#define BLOCK_SIZE    1024 * 100
+#define MY_WRAPPER
+
+static void* Wrap_MemoryBlock = malloc(BLOCK_SIZE * sizeof(char)); // nullptr;
+static uint16_t  Wrap_Offset{ 0 };
+
+
+extern std::atomic<uint32_t> Create;
+extern std::atomic<uint32_t> Delete;
+extern std::atomic<uint32_t> GotFuture;
+
+
+/* Non-blocking test of std::future to see if value is avalible yet */
+template<typename R>
+bool is_ready(std::future<R> const& f)
+{
+	return f.valid() ? f.wait_for(std::chrono::seconds(0)) == std::future_status::ready : false;
+}
+
+extern std::atomic<int> Function_Counter;
 namespace Core
 {
 	namespace Threading
 	{
+
+		struct Wrapper_Base
+		{/// __declspec(novtable) USE THIS
+			virtual ~Wrapper_Base() {
+			}//Print("Base");
+
+			virtual void Invoke() = 0;
+			void operator()()
+			{
+				Invoke();
+			}
+			/* Implement a Then, When_all and Deferred Function handling */
+
+			enum asyncStatus
+			{
+				Empty, Valid, Waiting, Busy, Submitted, Ready, Aquired
+			};
+
+			asyncStatus Status{ Empty };
+		};
+
+		template<typename _Func, typename ...ARGS>
+		struct asyncTask
+			: public Wrapper_Base
+		{
+			using type = std::invoke_result_t<std::decay_t<_Func&>, std::decay_t<ARGS>...>;
+			using reference_type = type&;
+			using Fptr = type(*)(ARGS...);
+
+			Fptr Function;
+			std::tuple<ARGS...> Arguments;
+			std::promise<type> ReturnValue;
+
+			int RefCount{ 0 };
+			asyncTask(_Func&& _function, ARGS&&... _args) noexcept
+				:
+				Function(std::move(_function)),
+				Arguments(std::forward<ARGS>(_args)...)
+			{
+				Status = Valid;
+			}
+			virtual ~asyncTask(){}
+
+			asyncTask(asyncTask&& _other) noexcept
+				:
+				Function(std::move(_other.Function)),
+				Arguments(std::forward<ARGS>(_other.Arguments)),
+				ReturnValue(std::move(_other.ReturnValue))
+			{
+				std::cout << "Called the Forward Function" << "\n";
+			}
+			asyncTask& operator=(asyncTask&& _other) noexcept
+			{
+				Function = std::move(_other.Function);
+				Arguments = std::forward<ARGS>(_other.Arguments);
+				ReturnValue = std::move(_other.ReturnValue);
+				std::cout << "Called Assignment Operator " << "\n";
+			}
+
+			virtual void Invoke()  override
+			{
+				Status = Busy;
+				auto result = std::apply(Function, Arguments);
+				ReturnValue.set_value(result);
+				Status = Waiting;
+			}
+
+			std::future<type> get_future()
+			{
+				Status = Submitted;
+				++Create;
+				GotFuture++;
+				RefCount++;
+				return ReturnValue.get_future();
+			}
+
+			type get()
+			{
+				Status = Aquired;
+				ReturnValue.get();
+			}
+
+			bool is_ready()
+			{
+				return ReturnValue.valid() ? (ReturnValue.wait_for(std::chrono::seconds(0)) == std::future_status::ready) : false;
+			}
+			asyncTask(const asyncTask&) = delete;
+			asyncTask& operator=(const asyncTask& _other) = delete;
+		};
 
 		struct JobQueue
 		{
 		public:
 			JobQueue() = default;
 
-			std::deque<std::packaged_task<void()>> TaskQueue;
-			std::vector<std::future<void>> finished;
-
-
 			std::condition_variable is_Ready;
 			std::mutex QueueMutex;
 			bool is_Done{ false };
-
-			bool Try_Pop(std::packaged_task<void()> &func);
-			
 			void Done();
-			bool pop(std::packaged_task<void()> &func);
-			template<typename _FUNC> bool try_push(_FUNC&& func)
+
+			std::deque<Wrapper_Base*> TaskQueue;
+
+			bool Try_Pop(Wrapper_Base*& func);
+			bool pop(Wrapper_Base*& func);
+
+			bool try_push(Wrapper_Base*func)
 			{
 				{
 					std::unique_lock<std::mutex> Lock{ QueueMutex, std::try_to_lock };
 					if (!Lock)
-					{
+					{/* If our mutex is already locked simply return */
 						return false;
 					}
-					TaskQueue.emplace_back(std::forward<_FUNC>(func));
-				}
-				is_Ready.notify_one();
-				return true;
+					/* Else place on the back of our Queue*/
+					TaskQueue.push_back(std::move(func));//(func));std::move<_FUNC>
+				}/* Unlock the Mutex */
+				is_Ready.notify_one(); /* Tell the world about it */
+				return true;/* Lets Async know u*/
 			}
-			template<typename _FUNC> void push(_FUNC&& func)
-			{
+			void push(Wrapper_Base* func)
+			{/* Adds a Function to our Queue */
 				{
 					std::unique_lock<std::mutex> Lock{ QueueMutex };
-					TaskQueue.emplace_back(std::forward<_FUNC>(func));
+					TaskQueue.emplace_back(std::move(func));//std::move<_FUNC> std::forward<_FUNC&>(&((Worker_Function *)
 				}
 			}
 		};
 
 		class ThreadPool
 		{
-			const size_t ThreadCount{ std::thread::hardware_concurrency() };
+			NO_COPY_OR_ASSIGNMENT(ThreadPool);
+
+			const unsigned int ThreadCount{  std::thread::hardware_concurrency() * 2};
 			std::vector<std::thread> Worker_Threads;
 			std::vector<JobQueue> ThreadQueue{ ThreadCount };
 			std::atomic<unsigned int> Index{ 0 };
 
-			ThreadPool();
-			~ThreadPool();
-// class _RET = std::result_of_t<_FUNC>,class _RET =std::result_of_t<_FUNC()>, 
+			ThreadPool(); ~ThreadPool();
 
 			void Run(unsigned int _i);
-		public:
 
+		public:
 			static ThreadPool &get()
 			{
 				static ThreadPool instance;
 				return instance;
 			}
+
 			template<typename _FUNC, typename...ARGS >
-			auto Async(_FUNC&& _func, ARGS&&...args)->std::future<decltype(_func(args...))>//  //
-			{
+			auto Async(_FUNC&& _func, ARGS&&... args)->std::future<typename asyncTask<_FUNC, ARGS... >::type>
+			{// Accept arbitrary Function signature and arguments into a Work pool for Asynchronous execution
+				auto _function = new asyncTask<_FUNC, ARGS... >(std::move(_func), std::forward<ARGS>(args)...);
+				auto result = _function->get_future();
+				
 				auto i = Index++;
-
-			//	std::function<decltype(_func(args...))()> func = ;
-				auto task_ptr = 
-					std::make_shared<std::packaged_task<decltype(_func(args...))()>>
-					(
-						std::bind
-						(
-							std::forward<_FUNC>(_func),
-							std::forward<ARGS>(args)...
-						)
-						);
-				std::function<void()> wrapper_func = [task_ptr]() {
-					(*task_ptr)();
-				};
-
-
-				int K = 8;	
-				for (unsigned int n = 0; n != ThreadCount * K; ++n) // K is Tunable 
+				int K = 1;
+				for (unsigned int n{ 0 }; n != ThreadCount * K; ++n) // K is Tunable 
 				{
-					if (ThreadQueue[(i + n) % ThreadCount].try_push(wrapper_func))
+					if (ThreadQueue[static_cast<size_t>((i + n) % ThreadCount)].try_push(static_cast<Wrapper_Base*>(_function)))
 					{
-						return task_ptr->get_future();//taskPTR->get_future();
+						return result;
 					}
 				}
-				 
-				ThreadQueue[i % ThreadCount].push(std::forward<std::function<void()>>(wrapper_func));
-				return  task_ptr->get_future();
+
+				ThreadQueue[i % ThreadCount].push(static_cast<Wrapper_Base*>(_function));
+				return result;
 			}
 		};
-
 	}// End NS Threading
 }// End NS Core 
+
+
+
+int TestFunctionA();
+int TestFunctionB(int _param);
+uint64_t TestFunctionLarge(uint64_t _p1, uint64_t _p2, uint64_t _p3, uint64_t _p4, uint64_t _p5);
+ 
+float TestFunctionC(float _paramA, int _paramB);
+
+std::string TestFunctionD(float _paramA, int _paramB);
+std::vector<uint32_t> TestFunctionE(int _paramA);
+std::vector<uint32_t> TestFunctionF(int _paramA);
+std::vector<uint32_t> TestFunctionG(int _paramA);
+std::vector<uint32_t> TestFunctionH(int _paramA);
+std::vector<uint32_t> TestFunctionI(int _paramA);
+std::vector<uint32_t> TestFunctionJ(int _paramA);
+
+uint64_t TestCompile(std::vector<std::vector<uint32_t>> _input);
+uint64_t Worker_TestFunction(size_t _count);
+
+#pragma warning( pop )
