2100c24bd10a6d346170c70cc901fd8038f5dfd8
Curtis Turner <curtis.turner@live.com>
2019-12-02 19:42:55 -0500

    Finished Threadpool

--- CreatureEngine/Core/Threading/Threadpool.cpp
+++ CreatureEngine/Core/Threading/Threadpool.cpp
@@ -1,23 +1,32 @@
 #include"Threadpool.h"
 
+#pragma warning( push )
+#pragma warning( disable : 4244 )
+#pragma warning( disable : 4018 ) // Optimization off warning of mine
+
+std::atomic<uint32_t> Create{ 0 };
+std::atomic<uint32_t> Delete{ 0 };
+std::atomic<uint32_t> GotFuture{ 0 };
+
+#define SLEEP_TIME 0
+std::atomic<int> Function_Counter{ 0 };
 
 namespace Core
 {
 	namespace Threading
 	{
-
-		bool JobQueue::Try_Pop(std::packaged_task<void()> &func)
+		bool JobQueue::Try_Pop(Wrapper_Base*& func)
 		{/* Try to aquire a function off the Queue to run */
 			std::unique_lock<std::mutex> Lock{ QueueMutex, std::try_to_lock };
 			if (!Lock || TaskQueue.empty())
 			{
 				return false;
 			}
-			func = std::move(TaskQueue.front());
-			TaskQueue.pop_front();
+			func = TaskQueue.front();//std::forward(TaskQueue.front());// std::move(
+		 	TaskQueue.pop_front();
 			return true;
 		}
-		bool JobQueue::pop(std::packaged_task<void()> &func)
+		bool JobQueue::pop(Wrapper_Base*& func)
 		{
 			std::unique_lock<std::mutex> Lock{ QueueMutex };
 			while (TaskQueue.empty() && !is_Done)
@@ -32,13 +41,11 @@ namespace Core
 			TaskQueue.pop_front();
 			return true;
 		}
-
-
-
 		void JobQueue::Done()
 		{
 			{
-				std::unique_lock<std::mutex> Lock{ QueueMutex }; is_Done = true;
+				std::unique_lock<std::mutex> Lock{ QueueMutex };
+				is_Done = true;
 			}
 			is_Ready.notify_all();
 		}
@@ -61,25 +68,349 @@ namespace Core
 				WT.join();
 			}
 		}
-
 		void ThreadPool::Run(unsigned int _i)
 		{
 			while (true)
 			{
-				std::packaged_task<void()> Func;
-				for (unsigned int N = 0; N != ThreadCount; ++N)
+				Wrapper_Base* Func{ nullptr };
+				for (unsigned int N{ 0 }; N != ThreadCount; ++N)
 				{
-					if (ThreadQueue[(size_t)(_i + N) % ThreadCount].Try_Pop(Func))
+					if (ThreadQueue[static_cast<size_t>((_i + N) % ThreadCount)].Try_Pop(Func))
 					{
 						break;
 					}
 				}
-				if (!ThreadQueue[_i].pop(Func))break;
-				Func();
+				if (!Func && !ThreadQueue[_i].pop(Func)){break;}
+				Func->Invoke();
+			 	delete &(*Func);
 			}
 		}
-
-
-
 	}// End NS Threading
 }// End NS Core
+
+
+#define Worker_Print(x) //std::cout << x << " : "
+
+/* unoptimized code section */
+OPTIMIZATION_OFF()
+
+void TestFunction(int _count, int _output)
+{
+	int result{ 0 };
+	for (int i{ 0 }; i < _count; ++i)
+	{
+		result++;
+		result += std::pow(result, _output);
+	}
+	std::cout << "PARAM:" << _output << "Finished Work: " << result << "\n";
+}
+OPTIMIZATION_ON()
+
+
+OPTIMIZATION_OFF()
+int TestFunctionA()
+{
+	Worker_Print( "FunctionA");
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return 11;
+}
+OPTIMIZATION_ON()
+
+
+OPTIMIZATION_OFF()
+int TestFunctionB(int _param)
+{
+	int result;
+	Worker_Print("FunctionB: " << _param) ;
+	//result.set_value(10);
+	result = _param * 2;
+	Sleep(SLEEP_TIME);
+++Function_Counter;
+	return result;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+float TestFunctionC(float _paramA, int _paramB)
+{
+	Worker_Print("FunctionC: " << _paramA << " : " << _paramB );
+	//Print("FuncC Param is " << _paramB << " : ");
+
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return _paramA * _paramB * (rand() % 1000);
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::string TestFunctionD(float _paramA, int _paramB)
+{
+	Worker_Print("FunctionD " << _paramA << " : " << _paramB );
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return "String: " + std::to_string(_paramA) + " : " + std::to_string(_paramB);
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionE(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+
+	uint64_t result{ 0 };
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		//	std::cout << "E: " << i*i ;
+		result += static_cast<int64_t>(i * i);
+		SomethingAllocated.push_back(result);
+	}
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionF(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+	uint64_t result{ 0 };
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		double A = sqrt(i*i);
+		//		std::cout << "F: " << A ;
+		result += static_cast<int64_t>(_paramA * i + A);
+		SomethingAllocated.push_back(result);
+	}	
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionG(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+	uint64_t result{ 0 };
+	int B = rand() % _paramA;
+	int C{ 0 };
+
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		double A = sqrt(i*i);
+		//		std::cout << "G: " << A ;
+		result += static_cast<int64_t>((_paramA * i) / pow(A, i));
+		SomethingAllocated.push_back(result);
+	}
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionH(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+	double A{ 0 };
+	uint64_t result{ 0 };
+	int B = rand() % _paramA;
+	int C{ 0 };
+
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		A = sqrt(i*i);
+	    Worker_Print( "H: " << A );
+		result += i * A;
+		SomethingAllocated.push_back(result);
+	}
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionI(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+	uint64_t A{ 0 };
+
+	int B = rand() % _paramA;
+	int C{ 0 };
+
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		A = pow(_paramA, i);
+		if (!((i+1) % (B+1)))C += A;
+		Worker_Print( "I: " << A );	
+		uint32_t result = A * C;
+		SomethingAllocated.push_back(result);
+	}
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+std::vector<uint32_t> TestFunctionJ(int _paramA)
+{
+	std::vector<uint32_t> SomethingAllocated;
+	float A = 0;
+	int B = rand() % _paramA;
+	int C{ 0 };
+	for (int i{ 0 }; i < _paramA; ++i)
+	{
+		A = pow(i, _paramA) / i;
+		if (A == B)C = i;
+		Worker_Print("J: " << A << ":");
+		uint32_t result = A * C;
+		SomethingAllocated.push_back(result);
+	}
+	Sleep(SLEEP_TIME);
+	++Function_Counter;
+	return SomethingAllocated;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+uint64_t Worker_TestFunction(size_t _count)
+{
+	int B = rand() % _count;
+	int C{ 0 };
+	for(uint32_t i{0}; i< _count; ++i)
+	{
+		std::cout << i << ":";
+		if (!((i+1) % (B+1)))C++;
+	}
+	return _count * C;
+}
+OPTIMIZATION_ON()
+
+OPTIMIZATION_OFF()
+uint64_t TestCompile(std::vector<std::vector<uint32_t>> _input)
+{
+	uint64_t result{ 0 };
+	for (auto& Y : _input)
+	{
+		for (auto& X : Y)
+		{
+			result += X;
+		}
+	}
+	return result;
+}
+OPTIMIZATION_ON()
+OPTIMIZATION_OFF()
+
+uint64_t TestFunctionLarge(uint64_t _p1, uint64_t _p2, uint64_t _p3, uint64_t _p4, uint64_t _p5)
+{
+	uint64_t result = _p1 + _p2 +_p3 +_p4 + _p5;
+	Print(result);
+	return result;
+}
+
+OPTIMIZATION_ON()
+
+#pragma warning( pop )
+
+/*NOTES:
+
+
+Intel Game Engine Design:
+https://software.intel.com/en-us/articles/designing-the-framework-of-a-parallel-game-engine
+
+
+Faster STD::FUNCTION Implementation
+https://github.com/skarupke/std_function/blob/master/function.h
+
+
+Lock Free Ring Buffer
+https://github.com/tempesta-tech/blog/blob/master/lockfree_rb_q.cc
+
+
+Lock-Free Programming
+https://www.cs.cmu.edu/~410-s05/lectures/L31_LockFree.pdf
+
+
+A Fast Lock-Free Queue for C++
+http://moodycamel.com/blog/2013/a-fast-lock-free-queue-for-c++
+
+
+Introduction to Multithreaded Algorithms
+http://ccom.uprrp.edu/~ikoutis/classes/algorithms_12/Lectures/MultithreadedAlgorithmsApril23-2012.pdf
+
+
+A Thread Pool with C++11
+http://progsch.net/wordpress/?p=81
+
+
+Parallelizing the Naughty Dog Engine
+https://www.gdcvault.com/play/1022186/Parallelizing-the-Naughty-Dog-Engine
+
+
+C++11 threads, affinity and hyperthreading
+https://eli.thegreenplace.net/2016/c11-threads-affinity-and-hyperthreading/
+
+
+Thread pool worker implementation
+https://codereview.stackexchange.com/questions/60363/thread-pool-worker-implementation
+
+
+Thread pool implementation using c++11 threads
+https://github.com/mtrebi/thread-pool
+
+
+C++11 Multithreading – Part 8: std::future , std::promise and Returning values from Thread
+https://thispointer.com/c11-multithreading-part-8-stdfuture-stdpromise-and-returning-values-from-thread/
+
+
+CppCon 2015: Fedor Pikus PART 2 “Live Lock-Free or Deadlock (Practical Lock-free Programming) ”
+Queue
+https://www.youtube.com/watch?v=1obZeHnAwz4&t=3055s
+
+
+Thread Pool Implementation on Github:
+https://github.com/mtrebi/thread-pool/blob/master/README.md#queue
+
+
+Threadpool with documentation:
+https://www.reddit.com/r/cpp/comments/9lvji0/c_threadpool_with_documentation/
+
+
+Original White paper on Work stealing Queues:
+http://supertech.csail.mit.edu/papers/steal.pdf
+
+
+Code overview - Thread Pool & Job System
+https://www.youtube.com/watch?v=Df-6ws_EZno
+
+
+Type Traits Reference
+https://code.woboq.org/llvm/libcxx/include/type_traits.html
+
+Aquiring results of a templated function:
+template<typename Function, typename ...Args>
+result_type = std::result_of_t<std::decay_t<Function>(std::decay_t<Args>...)>;
+
+MSVC Threadpool implementation for Concurrency:
+https://docs.microsoft.com/en-us/cpp/parallel/concrt/task-scheduler-concurrency-runtime?view=vs-2019
+
+
+Simple Threadpool Implementation:
+https://riptutorial.com/cplusplus/example/15806/create-a-simple-thread-pool
+
+
+Use this switch with Compiler Explorer inorder to allow it to compile: -std=c++17 -O3
+
+
+//template<typename _F, typename ...ARGS>
+//uint16_t Wrapper<_F, ARGS...>::Offset{ 0 };
+
+https://www.youtube.com/watch?v=zULU6Hhp42w&list=PLl8Qc2oUMko_FMAaK7WY4ly0ikLFrYCE3&index=4
+
+*/
--- CreatureEngine/main.cpp
+++ CreatureEngine/main.cpp
@@ -1,69 +1,257 @@
 #include<iostream>
-//#include"Common.h"
+#include<Windows.h>
+#pragma optimize( "", off )
+
+#define MY_WRAPPER
+#define _PROFILE_MEMORY
 
 #include"Profiling\SystemInfo.h"
 #include"Profiling\MemoryPerf\MemTracker.h"
 #include"Core\Threading\Threadpool.h"
+#include"Profiling\Timing\Benchmark.h"
+
+#include"Core\Common.h"
 
 using namespace Core;
 using namespace Threading;
 
+int LOOP_COUNT = 100;
 #include<string>
 
-int TestFunctionA()
+void  P0R0()
 {
-	std::cout << "FunctionA" << "\n";
-	return 11;
+	std::cout << "P0R0" << "\n";
 }
-int TestFunctionB(int _param)
+float P1R1(int _param)
 {
-	 int result;
-	std::cout << "FunctionB: " << _param << "\n";
-	//result.set_value(10);
-	result = _param * 2;
-	return result;
+	std::cout << "P1R1: " << _param << "\n";
+	return static_cast<float>(_param * _param);
 }
-float TestFunctionC(float _paramA, int _paramB)
+void  P1R0(int _param)
 {
-	std::cout << "FunctionC: " << _paramA << " : " << _paramB << "\n";
-	return _paramA * _paramB;
+	std::cout << "P1R0:" << _param << "\n";
+}
+void  P2R0(int _param, int _param2)
+{
+	std::cout << "P1R0:" << _param << ":" << _param2 << "\n";
+}
+int   P0R1()
+{
+	std::cout << "P0R1" << "\n";
+	return 42;
 }
 
-std::string TestFunctionD(float _paramA, int _paramB)
-{
-	std::cout << "FunctionD " << _paramA << " : " << _paramB << "\n";
-	return "String: " + std::to_string(_paramA) + " : " + std::to_string(_paramB);
-}
+#define NUMBER_OF_THREADS 2000
 
 
 int main()
 {
-	auto Test4 = ThreadPool::get().Async(TestFunctionD,123.321f, 10);
-	auto Test1 = ThreadPool::get().Async(TestFunctionB, 1431);
-	auto Test3 = ThreadPool::get().Async(TestFunctionA);
-	auto Test2 = ThreadPool::get().Async(TestFunctionC, 3.14159f, 123);
-
-	std::cout << "Return Value A:" << Test1.get() << "\n";
-	std::cout << "Return Value B:" << Test2.get() << "\n";
-	std::cout << "Return Value C:" << Test3.get() << "\n";
-	std::cout << "Return Value D:" << Test4.get() << "\n";
-	Profiling::SystemInfo::Retrieve_SystemInfo();
+	while (true)
 	{
-		int *A = new int();
-		delete(A);
+		//alloca()
+		Function_Counter = 0;
+		LOOP_COUNT += 1000;// 22100 is when Threadpool and Linear start to become one.
+		Print("\n\n\n\n Loop Counter:" << LOOP_COUNT << " iterations in the Worker Functions\n");
+
+		Profiling::Timing::Profile_Timer<100> Bench;
+		{
+			std::vector<std::vector<uint32_t>> Test;
+
+			auto A = ThreadPool::get().Async(TestFunctionE, std::move(LOOP_COUNT));
+			auto B = ThreadPool::get().Async(TestFunctionE, std::move(LOOP_COUNT));
+			auto C = ThreadPool::get().Async(TestFunctionB, 1431);
+			auto D = ThreadPool::get().Async(TestFunctionD, 123.321f, 10);
+			auto E = ThreadPool::get().Async(TestFunctionA);
+			auto F = ThreadPool::get().Async(TestFunctionC, 3.14159f, 123);
+			auto G = ThreadPool::get().Async(TestFunctionF, std::move(LOOP_COUNT));
+			auto H = ThreadPool::get().Async(TestFunctionG, std::move(LOOP_COUNT));
+			auto I = ThreadPool::get().Async(TestFunctionH, std::move(LOOP_COUNT));
+			auto J = ThreadPool::get().Async(TestFunctionI, std::move(LOOP_COUNT));
+			auto K = ThreadPool::get().Async(TestFunctionJ, std::move(LOOP_COUNT));
+
+			Print("Thread Pool Cluster");
+
+			std::vector<std::future<float>> Fut;
+			for (int i{ 0 }; i < NUMBER_OF_THREADS; ++i)
+			{
+				auto F = ThreadPool::get().Async(TestFunctionC, 123.321f, std::move(i));
+				Fut.push_back(std::forward<std::future<float>>(F));
+			}	
+
+			uint64_t result{ 0 };
+			int counter = Fut.size();
+			while (counter)
+			{
+				for (auto& F : Fut)
+				{
+					if (!is_ready(F)) continue;
+					result += F.get(); 
+					--counter;
+				}
+			}
+
+			Print("End Thread Pool Cluster: " << result);
+
+
+			while (Function_Counter < 10) {/* SpinLock until every single function called returns as measured via the atomic int Function_Counter. */ }
+
+		 	Print("Threadpool: " << result);
+ 			Bench.Stop();
+		}
+ 
+		Function_Counter = 0;
+		Profiling::Timing::Profile_Timer<100> ThreadBM;
+		{
+			auto  TPTest5T = std::async(std::launch::async | std::launch::deferred, TestFunctionE, LOOP_COUNT);
+			auto  TPTest1T = std::async(std::launch::async | std::launch::deferred, TestFunctionB, 1431);
+			auto  TPTest4T = std::async(std::launch::async | std::launch::deferred, TestFunctionD, 123.321f, 10);
+			auto  TPTest3T = std::async(std::launch::async | std::launch::deferred, TestFunctionA);
+			auto  TPTest2T = std::async(std::launch::async | std::launch::deferred, TestFunctionC, 3.14159f, 123);
+			auto  TPTest6T = std::async(std::launch::async | std::launch::deferred, TestFunctionF, LOOP_COUNT);
+			auto  TPTest7T = std::async(std::launch::async | std::launch::deferred, TestFunctionG, LOOP_COUNT);
+			auto  TPTest8T = std::async(std::launch::async | std::launch::deferred, TestFunctionH, LOOP_COUNT);
+			auto  TPTest9T = std::async(std::launch::async | std::launch::deferred, TestFunctionI, LOOP_COUNT);
+			auto TPTest10T = std::async(std::launch::async | std::launch::deferred, TestFunctionJ, LOOP_COUNT);
+			auto TPTest11T = new  asyncTask([](int)->int { Print("Lambda Function Call std::Async"); return 11; }, 10000);
+
+			Print("Async Cluster");
+			std::vector<std::future<float>> Fut;
+	        for (int i{ 0 }; i < NUMBER_OF_THREADS; ++i)
+	        {
+	        	auto TPTest4loop = std::async(std::launch::async, TestFunctionC, 123.321f, i);
+				Fut.push_back(std::move(TPTest4loop));
+	        }	
+			Print("End Async Cluster");
+
+
+			uint64_t result{ 0 };
+			int counter = Fut.size();
+			while (counter)
+			{
+				for (auto& F : Fut)
+				{
+					if (!is_ready(F))
+					{// if not ready yet, check the next  
+						continue;
+					}
+					result += F.get(); // it is ready 
+					--counter;
+				}
+			}
+			Print("Async :" << result);
+			while (Function_Counter < 10) {// SpinLock until every single function called returns as measured via the atomic int Function_Counter. 
+ }
+			std::vector<std::vector<uint32_t>> Test;
+
+			Test.push_back(TPTest5T.get());
+			Test.push_back(TPTest6T.get());
+			Test.push_back(TPTest7T.get());
+			Test.push_back(TPTest8T.get());
+			Test.push_back(TPTest9T.get());
+			Test.push_back(TPTest10T.get());
+			Print("Async: " << TPTest4T.get() << " : " << TestCompile(Test));
+
+			ThreadBM.Stop();
+		}
+
+
+		Function_Counter = 0;
+		Profiling::Timing::Profile_Timer<100> LBench;
+		{
+			auto Test5 = TestFunctionE(std::move(LOOP_COUNT));// .5ms
+			auto Test4 = TestFunctionD(123.321f, 10);
+			auto Test1 = TestFunctionB(1431);
+			auto Test3 = TestFunctionA();
+			auto Test2 = TestFunctionC(3.14159f, 123);
+			auto Test6 = TestFunctionF(std::move(LOOP_COUNT));//.3
+			auto Test7 = TestFunctionG(std::move(LOOP_COUNT));//3.21
+			auto Test8 = TestFunctionH(std::move(LOOP_COUNT));// .32
+			auto Test9 = TestFunctionI(std::move(LOOP_COUNT));//2.8
+			auto Test10 = TestFunctionJ(std::move(LOOP_COUNT));//2.6
+			auto Test11 = [](int)->int { Print("Lambda Function Linear call"); return 11; };
+
+
+			while (Function_Counter > 10) {// SpinLock until every single function called returns as measured via the atomic int Function_Counter. 
+			}
+
+			uint64_t result{ 0 };
+			for (int i{ 0 }; i < NUMBER_OF_THREADS; ++i)
+			{
+				result += TestFunctionC( 123.321f, rand()% NUMBER_OF_THREADS);
+			}
+			Print("Linear :" << result);
+			
+
+			std::vector<std::vector<uint32_t>> Test;
+			Test.push_back(Test5);
+			Test.push_back(Test6);
+			Test.push_back(Test7);
+			Test.push_back(Test8);
+			Test.push_back(Test9);
+			Test.push_back(Test10);
+			Print("Linear: " << Test4 << " : " << TestCompile(Test));
+
+			LBench.Stop();
+		}
+
+		std::cout << " Straight Linear = " << LBench.Results / 1000.0f << " ms" << "\n";//0.020034
+		std::cout << "     Thread Pool = " << Bench.Results / 1000.0f << " ms" << "\n";
+		std::cout << "      std::async = " << ThreadBM.Results / 1000.0f << " ms" << "\n";//0.020034
+		Sleep(1500);
 	}
+
+	//	Profiling::Memory::TrackDumpBlocks();
+	//	Profiling::Memory::TrackListMemoryUsage();
 	return 0;
 }
+OPTIMIZATION_ON()
 
 
-/*
-Googles Class Structure:
----
-Typedefs, Using, and Enums,
-Constants,
-Constructors,
-Destructor, 
-Methods(including static methods),
-Data Members( including static data members)
 
+
+/*    Apply tuple to a function: https://cpppatterns.com/patterns/apply-tuple-to-function.html    */
+template<typename _FUNC, typename _TUPLE, size_t ..._SZ >
+auto apply_tuple_impl(_FUNC&& _function, _TUPLE&& _arguments, std::index_sequence<_SZ...>)
+->decltype((std::forward<_FUNC>(_function)(std::get<_SZ>(std::forward<_TUPLE>(_arguments))...)))/// Return was Decltype(auto) just auto and trailing return added to create zero/low cost abstraction
+{
+	return std::forward<_FUNC>(_function)(std::get<_SZ>(std::forward<_TUPLE>(_arguments))...);
+}
+
+template<typename _FUNC, typename _TUPLE>
+decltype(auto) apply_from_tuple(_FUNC&& _function, _TUPLE&& _tupleArguments)
+{
+	std::size_t constexpr tSize
+	{// Initialize tSize instead of using =
+		 std::tuple_size<typename std::remove_reference<_TUPLE>::type>::value
+	};
+
+	return
+		apply_tuple_impl(std::forward<_FUNC>(_function),
+		std::forward<_TUPLE>(_tupleArguments),
+		std::make_index_sequence<tSize>());
+}
+
+/* 
+=====================================================================================================================
+									  NOTES:                                                                           
+=====================================================================================================================
+Pragmas for C++ Compilers, Good resource
+https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.cbclx01/prag_ishome.htm
+
+ Open STD:
+ http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2014/n3857.pdf 
+
+ 
+Physics Solver: 
+https://www.gdcvault.com/play/1013359/High-Performance-Physics-Solver-Design
+
+
+
+=====================================================================================================================
 */
+
+
+
+
+
+
