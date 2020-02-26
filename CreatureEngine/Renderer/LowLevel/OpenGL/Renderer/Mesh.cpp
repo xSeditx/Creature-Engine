c4fcafbc1fe4c44c3d35758a19c50af8d80c6ab2
Sedit <none>
2020-02-26 18:46:25 -0500

    Moving from Laptop to PC

--- CreatureEngine/Renderer/LowLevel/OpenGL/Renderer/Mesh.cpp
+++ CreatureEngine/Renderer/LowLevel/OpenGL/Renderer/Mesh.cpp
@@ -9,9 +9,8 @@ Mesh::Mesh()
 	:
 	GameObject(Vec3(0), Vec3(0), Vec3(1.0f)),
 	MaterialID(NULL), 
-	VAO({ 0 })
-{
-}
+	VAO(nullptr)
+{}
 
 
 Mesh::Mesh(std::string file)
@@ -19,7 +18,7 @@ Mesh::Mesh(std::string file)
 	GameObject(Vec3(0), Vec3(0), Vec3(1.0f)),
 	Filepath(file),
 	MaterialID(NULL),
-	VAO({0})
+	VAO(nullptr)
 {
 	__debugbreak();
 }
@@ -66,24 +65,25 @@ void Mesh::Update()
 {//  Set Identity and Rotate all axis followed with the Translation.
 
 
-	Transform = glm::rotate
-	( // ensure unneeded copying as the results from the last transform are immediately used in the next step
+	Transform =
 		glm::rotate
-		(
+		( // ensure unneeded copying as the results from the last transform are immediately used in the next step
 			glm::rotate
 			(
-				glm::translate
-				( // Starts with the Translation here
-					glm::mat4(1.0f),
-					Position
-				),
-				glm::radians(Rotation.x),
-				Vec3(1.0f, 0.0f, 0.0f)),
-			glm::radians(Rotation.y),
-			Vec3(0.0f, 1.0f, 0.0f)),
-		glm::radians(Rotation.z),
-		Vec3(0.0f, 0.0f, 1.0f)
-	);
+				glm::rotate
+				(
+					glm::translate
+					( // Starts with the Translation here
+						glm::mat4(1.0f),
+						Position
+					),
+					glm::radians(Rotation.x),
+					Vec3(1.0f, 0.0f, 0.0f)),
+				glm::radians(Rotation.y),
+				Vec3(0.0f, 1.0f, 0.0f)),
+			glm::radians(Rotation.z),
+			Vec3(0.0f, 0.0f, 1.0f)
+		);
 	needsUpdated = false; // Tell Application we Finished Updating. May need to make this Atomic in the future.
 	// #ATOMIC NEEDED
 }
