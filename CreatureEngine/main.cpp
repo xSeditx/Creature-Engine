b293ca9011f18282fec7f50f25b25ecec9363e55
Sedit <none>
2020-03-05 12:48:06 -0500

    Broken FlipXaxis in Bitmap.cpp

--- CreatureEngine/main.cpp
+++ CreatureEngine/main.cpp
@@ -284,7 +284,7 @@ int main()
  	Print("Viewport: " << vp.x << " : " << vp.y << " : " << vp.z << " : " << vp.w );
 
 	MyApp.Run();
-	MyApp.End();
+
 
 	//	Profiling::Memory::TrackDumpBlocks();
 	//	Profiling::Memory::TrackListMemoryUsage();
